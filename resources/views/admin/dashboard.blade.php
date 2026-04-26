b97b8bc835a9e6f2f7197982aac5fdb36bf60795
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-04-26 16:57:06 +0430

    the sweetalert is done

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -40,13 +40,17 @@
         </div>
     </div>
 
-      <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+    <script src="{{ asset('backend/assets/js/code.js') }}"></script>
 
       
     <script src="{{ asset('backend/assets/js/bundle.js') }}"></script>
     <script src="{{ asset('backend/assets/js/scripts.js') }}"></script>
 
-     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
 <script>
  @if(Session::has('message'))
