06f3b88277cb13f9a1f69d413b71f5e007716d49
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-04-27 14:07:00 +0430

    the db seed and feature is create

--- database/migrations/2026_04_26_060225_create_homes_table.php
+++ database/migrations/2026_04_26_060225_create_homes_table.php
@@ -19,6 +19,15 @@ return new class extends Migration
             $table->string("image")->nullable();
             $table->timestamps();
         });
+
+         Schema::create('features', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->text("description");
+            $table->string("image")->nullable();
+            $table->timestamps();
+        });
+
     }
 
     /**
