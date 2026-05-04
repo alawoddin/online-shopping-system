93184d973e525cf47203ad96c58c70e7d305606f
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-05-04 14:52:30 +0430

    Add commit  1

---
+++ log.bat
@@ -0,0 +1,12 @@
+@echo off
+cd /d C:\your-project-path
+
+for /l %%x in (1, 1, 10) do (
+    echo i commit the code but not show the commit it is scure %%x >> log.bat
+    git add .
+    git commit -m "Add commit  %%x"
+)
+
+git push origin main
+pauseUpdate 1 
+i commit the code but not show the commit it is scure 1 
