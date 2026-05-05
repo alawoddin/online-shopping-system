b4f5103571f158781611badc665af8539bb8c598
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-05-05 15:31:46 +0430

    the home section is dynamic in frontend

--- resources/views/frontend/layout/banner.blade.php
+++ resources/views/frontend/layout/banner.blade.php
@@ -1,18 +1,22 @@
 <section class="banner-area">
+
+  @php
+
+  $homes = \App\Models\Home::all();
+    
+  @endphp
       <div class="container">
         <div class="row fullscreen align-items-center justify-content-start">
           <div class="col-lg-12">
             <div class="active-banner-slider owl-carousel">
               <!-- single-slide -->
-              <div class="row single-slide align-items-center d-flex">
+              @foreach ($homes as $item)
+                       <div class="row single-slide align-items-center d-flex">
                 <div class="col-lg-5 col-md-6">
                   <div class="banner-content">
-                    <h1>Nike New <br />Collection!</h1>
+                    <h1>{{$item->title}}</h1>
                     <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud
-                      exercitation.
+                      {{ $item->description }}
                     </p>
                     <div class="add-bag d-flex align-items-center">
                       <a class="add-btn" href=""
@@ -26,41 +30,16 @@
                   <div class="banner-img">
                     <img
                       class="img-fluid"
-                      src="{{ asset('frontend/assets/img/banner/banner-img.png') }}"
+                       src="{{ !empty($item->image) ? asset($item->image) : asset('uploads/no_image.png') }}"
                       alt=""
                     />
                   </div>
                 </div>
               </div>
+              @endforeach
+       
               <!-- single-slide -->
-              <div class="row single-slide">
-                <div class="col-lg-5">
-                  <div class="banner-content">
-                    <h1>Nike New <br />Collection!</h1>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud
-                      exercitation.
-                    </p>
-                    <div class="add-bag d-flex align-items-center">
-                      <a class="add-btn" href=""
-                        ><span class="lnr lnr-cross"></span
-                      ></a>
-                      <span class="add-text text-uppercase">Add to Bag</span>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-lg-7">
-                  <div class="banner-img">
-                    <img
-                      class="img-fluid"
-                      src="{{ asset('frontend/assets/img/banner/banner-img.png') }}"
-                      alt=""
-                    />
-                  </div>
-                </div>
-              </div>
+            
             </div>
           </div>
         </div>
